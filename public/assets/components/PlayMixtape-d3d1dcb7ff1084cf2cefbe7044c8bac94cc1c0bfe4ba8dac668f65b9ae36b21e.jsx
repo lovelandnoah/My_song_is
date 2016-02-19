2db1bca53b11f3e16c9c6776211e365127b5f181
Sam Hammer <sam@michaelrobertson.com>
2016-02-19 10:51:09 -0800

    compiling issue

---
+++ public/assets/components/PlayMixtape-d3d1dcb7ff1084cf2cefbe7044c8bac94cc1c0bfe4ba8dac668f65b9ae36b21e.jsx
@@ -0,0 +1,137 @@
+  class PlayMixtape extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = { searched: false, mixtape_id: this.props.mixtape_id, mixtapeName: '', mixTapeCategory: '', songs: [], songsSearchedFor: [], results: []};
+    this.getSongs = this.getSongs.bind(this);
+
+
+
+    this.getSearchResults = this.getSearchResults.bind(this);
+    this.noArtists = this.noArtists.bind(this);
+    this.pass = this.pass.bind(this);
+    this.showSuggestions = this.showSuggestions.bind(this);
+  }
+
+  componentDidMount(){
+    this.getSongs();
+
+    self = this;
+    self.showSuggestions();
+  }
+
+
+  // play mixtape original
+  getSongs(){
+    $.ajax({
+      url: '/mixtapes_find_single_mixtape',
+      type: 'GET',
+      data: {mixtape_id: this.props.mixtape_id}
+    }).success( data => {
+      this.setState({mixtapeName: data.name});
+      this.setState({songs: data.songs});
+    })
+  }
+
+   getSearchResults(){
+    let self = this;
+    let searchTerm = self.refs.searchText.value.replace(/\s/g, "%20")
+    $.ajax({
+      url: "http://api.dar.fm/playlist.php?&q=" + searchTerm + "&callback=jsonp&web=1&partner_token=9388418650",
+      jsonp: 'callback',
+      type: 'GET',
+      dataType: 'jsonp',
+    }).success( data => {
+      this.state.searched = true;
+      this.setState({results: data});
+    });
+  }
+
+  showSuggestions(){
+    $("#search").autocomplete( {
+      source(request, response){ 
+        $.ajax({
+          url: "http://api.dar.fm/songartist.php?q=*" + request.term + "*&callback=jsonp&web=1&partner_token=9388418650",
+          jsonp: "callback",
+          type: "GET",
+          dataType: "jsonp",
+        }).success( data => {
+            response(data)
+        });
+      }
+    });
+  }
+
+   pass(){
+    $("#mixtapeForm").slideToggle("slow");
+    $("#cardHolder").slideToggle("slow");
+  }
+
+  noArtists(artists){
+    if(artists.length == 0 && this.state.searched) {
+      if(self.refs.searchText.value == "") {
+        return(<h5>Please search for an artist! </h5>);
+      }else {
+        return(<h5>Could not find {self.refs.searchText.value} playing on a station.</h5>);
+      }
+    }
+  }
+
+  render(){
+
+    self = this;
+    let i = 0;
+    let artists = this.state.results.map( artist => {
+      let key = `artist-${i += 1}`
+      return(<Artist key={key} {...artist} rplay={self.playSong} mixtapeId={self.state.mixtape_id} getSongs={self.getSongs}/>);
+    });
+
+    // let songArray = [];
+    let songsSearchedFor = this.state.songsSearchedFor.map( song => {
+      let key = `songsSearchedFor-${song.song_id}`
+      songsSearchedFor.push(<MixTapeSong key={key} {...song}/>);
+    });
+
+    let songs = this.state.songs.map( song => {
+    let key = `song-${song.song_id}`;
+
+    return(<Song key={key} artist_name={this.props.artist_name} song_name={this.props.song_name} {...song} />);
+
+  });
+
+    return(<div>
+            <div className= 'card-panel cyan' id="playing-mixtape">
+              <div className='card-content white-text'>
+                <h5 className="center">{this.state.mixtapeName} </h5>
+                {songs}
+              </div>
+            </div>
+
+            <div id="mixtapeForm">
+            </div>
+          
+            <br />
+            <h5 className="subtit white-text salt center">Search for an Artist or Song:</h5>
+            <input id='search' type='text' ref='searchText' autofocus='true' placeholder="Artist"/>
+            <div className="center">
+              <button onClick={this.getSearchResults} className='btn waves-effect waves-light'>Search</button>
+            </div>
+            <br />
+            <br />
+            <h4 className='center-align center subtit white-text salt'>Songs Playing:</h4>
+            <br />
+            <div className='row'>
+              {this.noArtists(artists)}
+              {artists}
+            </div>
+
+
+
+
+          </div>
+
+
+
+          )
+          
+  }
+}
---
+++ public/assets/components/TopArtist-08a96ffeca544e09b95aedfb67af98ab1e5605f5ba59a2dcd07a6eb96e446365.jsx
@@ -0,0 +1,65 @@
+class TopArtist extends React.Component{
+  constructor(props){
+    super(props)
+    this.play = this.play.bind(this)
+    this.add = this.add.bind(this)
+  }
+  componentDidMount(){
+  }
+
+  play(station){
+    let player = document.getElementById("player")
+    player.src = "http://api.dar.fm/player_api.php?station_id=" + station + "&custom_style=radioslice&partner_token=9388418650"
+  }
+
+
+  mobilePlayButton(station){
+    song = this.props.songtitle.replace(/\s/g, ".")
+    artist = this.props.songartist.replace(/\s/g, ".")
+
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      window.open("http://onrad.io/" + artist + "." + song)
+    } else {
+      this.play(station);
+    } 
+  }
+
+  add(songName, artist){
+    let self = this;
+    // mixtape = Mixtape.where(user_id: current_user.id)
+    $.ajax({
+      url: '/song',
+      type: 'POST',
+      data: {name: songName, artist: artist, mixtape_id: mixtape}
+
+    }).success( data => {
+
+      self.props.getSongs();
+
+    });
+  }
+
+
+
+  
+
+  render(){
+    return(<div>
+            <div className="nav4 card-panel height cyan z-depth-3">
+              <div className="card-content purple">
+                <div className="stylez center truncate">
+                  <h4 className="salt white-text"># {this.props.rank}</h4>
+                  <h3 className="salt white-text">{this.props.songtitle}</h3>
+                  <h4 className="salt white-text">By: {this.props.songartist}</h4>
+                </div>
+                <div className="row center">
+                	<br />
+                  <a className="btn waves-effect waves-light marg" onClick={() => this.mobilePlayButton(this.props.station_id)}>play</a>
+                  <a className="btn bluezs waves-effect waves-light" onClick={() => this.add(this.props.title, this.props.artist)}>Add</a>
+                </div>
+                <br />
+              </div>
+            </div>
+          </div>);
+  }
+}
