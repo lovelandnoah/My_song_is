c342807e82581ace126f1c9d19e41a7844f69119
Noah Loveland <lovelandnoah@gmail.com>
2016-02-19 22:24:19 -0700

    delete button working plus fixed up some things

--- app/assets/javascripts/components/PlayMixtape.js.jsx
+++ app/assets/javascripts/components/PlayMixtape.js.jsx
@@ -95,7 +95,7 @@
     let songs = this.state.songs.map( song => {
     let key = `song-${song.song_id}`;
 
-    return(<Song key={key} artist_name={this.props.artist_name} song_name={this.props.song_name} {...song} />);
+    return(<Song key={key} artist_name={this.props.artist_name} song_name={this.props.song_name} {...song} getSongs={this.getSongs} />);
 
   });
 
