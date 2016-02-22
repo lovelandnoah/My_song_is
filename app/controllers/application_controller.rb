7547184479cc4e8ae477c815d93115a8ad42dc80
Sam Hammer <sam@michaelrobertson.com>
2016-02-22 11:46:18 -0800

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   # this is hopefully to get usernames working
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password,:username) }
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,:username) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation,:username) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :username) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :username) }
   end
 
   def ensure_signup_complete
