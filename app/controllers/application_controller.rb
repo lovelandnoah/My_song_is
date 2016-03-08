64fdf39095daf82c36f0de43075b5d811323b6c9
Sam Hammer <sam@michaelrobertson.com>
2016-03-08 12:11:53 -0800

    image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password,:username) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :username) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :username) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :username, :full_name, :picture) }
   end
 
   def ensure_signup_complete
