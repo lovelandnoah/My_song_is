66b8441d207382fb2cd91274e9693a9f13f4b141
Sam Hammer <sam@michaelrobertson.com>
2016-05-03 08:54:16 -0700

    include assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,9 +9,5 @@ Rails.application.config.assets.precompile += %w( components.js )
 
 Rails.application.config.assets.precompile += %w( jquery.validate.js )
 Rails.application.config.assets.precompile += %w( jquery.validate.additional-methods.js )
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( validation-extentions.js )
+# config.assets.precompile += %w(*.js)
