0c93941eeea67b5ee268f63556d98cf6296d4925
Sam Hammer <sam@michaelrobertson.com>
2016-05-03 08:09:10 -0700

    compile

--- public/assets/application-e6dbcc31a256a9d3adc0c4d31bd5085d394891d4ddd25e719afba17b117e23ff.js
+++ public/assets/application-e6dbcc31a256a9d3adc0c4d31bd5085d394891d4ddd25e719afba17b117e23ff.js
@@ -62052,6 +62052,25 @@ $.validator.addMethod("ziprange", function(value, element) {
 }, "Your ZIP-code must be in the range 902xx-xxxx to 905xx-xxxx");
 
 }));
+jQuery.extend(jQuery.validator.messages, {
+    required: "This information is required.",
+    remote: "This is already taken!",
+    email: "Please enter a valid email address.",
+    url: "Please enter a valid URL.",
+    date: "Please enter a valid date.",
+    dateISO: "Please enter a valid date (ISO).",
+    number: "Please enter a valid number.",
+    digits: "Please enter only digits.",
+    creditcard: "Please enter a valid credit card number.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
 //     Underscore.js 1.4.4
 //     http://underscorejs.org
 //     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud Inc.
@@ -64175,6 +64194,7 @@ if (typeof JSON !== 'object') {
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+
 
 
 
